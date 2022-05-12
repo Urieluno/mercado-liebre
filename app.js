38adde6696eff1f5d6c8797af2a53191eac14f11
Urieluno <urielperrenfernandez@gmail.com>
2022-05-11 23:30:52 -0300

    Mercado Liebre primer version

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//Servidor
+app.listen(8080, () => {
+console.log('Servidor corriendo correctamente en el puerto 8080')})
+
+//Static
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Rutas
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+});
